ad2ac858469443a958ef103373c180e15ad881d9
Lochlann O Neill <47467279+Lochlannoneill@users.noreply.github.com>
2022-11-20 15:33:40 +0000

    completed assignment
    everything completed, including documentation

--- lochlann.py
+++ lochlann.py
@@ -9,6 +9,7 @@ import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -18,32 +19,40 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 import warnings
 
+
 warnings.filterwarnings('ignore')
-SAMPLE_SIZE = 600
+#SAMPLE_SIZE = 600
+SAMPLE_SIZES = [800, 1600, 3200, 6400, 12800]
 KFOLD_SIZE = 10
 
 
 def avg(list):
     return sum(list) / len(list)
 
+
 def plot_image(image):
+    
     plt.imshow(np.array(image).reshape(28, 28))
     plt.show()
 
 def preprocess_data(filename):
+    
     df_images = pd.read_csv(filename)
     features = df_images.drop("label", axis=1)
     labels = df_images["label"]
     return features, labels
 
 def parameterize_data(param_size, features, labels):
+    
     features_param = features.sample(param_size)
     labels_param = labels[features_param.index]
     return features_param, labels_param
 
-def evaluate_classifier(size_kfolds, classifier, features, labels):
+def evaluate_classifier(size_kfolds, classifier, sample_size, features, labels):
+    
     print("\n\n------------------------------------------------")
-    print("\n------------", classifier, "------------")
+    print("\n------------", classifier)
+    print("\n------------ Sample Size:\t", sample_size)
     print("\n------------------------------------------------")
     
     summary = {
@@ -105,12 +114,14 @@ def evaluate_classifier(size_kfolds, classifier, features, labels):
    
     return summary
         
-def evaluate_perceptron(features, labels):
+def evaluate_perceptron(sample_size, features, labels):
+    
     classifier = linear_model.Perceptron()
-    summary = evaluate_classifier(KFOLD_SIZE, classifier, features, labels)
+    summary = evaluate_classifier(KFOLD_SIZE, classifier, sample_size, features, labels)
     return summary
 
-def evaluate_SVM(features, labels):
+def evaluate_SVM(sample_size, features, labels):
+    
     #use a radial basis function kernel
     #gammas = [1e-1, 1e-3, 1e-5, 1e-7]
     #for gamma in gammas:
@@ -118,10 +129,10 @@ def evaluate_SVM(features, labels):
     #    summary = evaluate_classifier(KFOLD_SIZE, classifier, features, labels)
     
     classifier = svm.SVC(kernel="rbf", gamma=1e-7)
-    summary = evaluate_classifier(KFOLD_SIZE, classifier, features, labels)
+    summary = evaluate_classifier(KFOLD_SIZE, classifier, sample_size, features, labels)
     return summary # return summary of last evaluation (1e-7), prediction accuracy > 90%
 
-def evaluate_knearest(features, labels): # FIXME
+def evaluate_knearest(sample_size, features, labels): # FIXME
 
     #neighbours = [1, 3, 5, 7, 9, 11]
     #for neighbour in neighbours:
@@ -129,13 +140,14 @@ def evaluate_knearest(features, labels): # FIXME
     #    summary = evaluate_classifier(KFOLD_SIZE, classifier, features, labels)
     
     classifier = KNeighborsClassifier(n_neighbors=3) # FIXME - why is there an error message in the terminal, despite working fine?????
-    summary = evaluate_classifier(KFOLD_SIZE, classifier, features, labels)
+    summary = evaluate_classifier(KFOLD_SIZE, classifier, sample_size, features, labels)
     return summary
 
-def evaluate_dtree(features, labels):
+def evaluate_dtree(sample_size, features, labels):
     classifier = DecisionTreeClassifier()
-    summary = evaluate_classifier(KFOLD_SIZE, classifier, features, labels)
+    summary = evaluate_classifier(KFOLD_SIZE, classifier, sample_size, features, labels)
     return summary
+
 
 
 def main():
@@ -147,12 +159,9 @@ def main():
     # COMPLETED    -   how many samples are images of sneakers, how many sameples are images of ankle boots [1point]
     # COMPLETED    -   display at least one for each class [2point]
     print("\n\n------------------------------------------------")
-    print("\n--------------- Pre-process data ---------------")
+    print("\n--------------- Pre-process data")
     print("\n------------------------------------------------")
     features, labels = preprocess_data('product_images.csv')
-    
-    #print("\nData = FEATURE VECTORS\nType = ", type(features), features.head())
-    #print("\nData = LABELS\nType = ", type(labels), labels.head())
     
     print("{:<8s}:\t{:>}".format("Data", "FEATURE VECTORS"))
     print("{:<8s}:\t".format("Type"), end="")
@@ -191,16 +200,28 @@ def main():
     # COMPLETED    -         the training time per training sample [1point]
     # COMPLETED    -         the prediction time per evaluation sample [1point]
     # COMPLETED    -         the prediction accuracy [1point]
-    print("\n\n--------------------------------------------------")
-    print("\n----------- Parameterize Sample Size-----------")
-    print("\n--------------------------------------------------")
-    features_param, labels_param = parameterize_data(SAMPLE_SIZE, features, labels)
+    list_summary_perceptron = []
+    list_summary_SVM = []
+    list_summary_knearest = []
+    list_summary_dtree = []
     
-    print("{:<32s}:\t{:>}".format("PARAMETERIZED DATA SAMPLE SIZE", SAMPLE_SIZE))
-    print("{:<32s}:\t{:>}".format("Count Feature Vectors (param)", features_param.size))
-    print("{:<32s}:\t{:>}".format("Count Labels (param)", labels_param.size))
-    print("{:<32s}:\t{:>}".format("Count Sneakers (param)", labels_param[labels_param == 0].size))
-    print("{:<32s}:\t{:>}".format("Count Ankle Boots (param)", labels_param[labels_param == 1].size))
+    for sample_size in SAMPLE_SIZES:
+        features_param, labels_param = parameterize_data(sample_size, features, labels)
+        
+        print("\n\n--------------------------------------------------")
+        print("\n----------- New Parameterized Sample Size")
+        print("\n----------- Sample Size:\t", sample_size)
+        print("\n--------------------------------------------------")
+        print("{:<32s}:\t{:>}".format("PARAMETERIZED DATA SAMPLE SIZE", sample_size))
+        print("{:<32s}:\t{:>}".format("Count Feature Vectors (param)", features_param.size))
+        print("{:<32s}:\t{:>}".format("Count Labels (param)", labels_param.size))
+        print("{:<32s}:\t{:>}".format("Count Sneakers (param)", labels_param[labels_param == 0].size))
+        print("{:<32s}:\t{:>}".format("Count Ankle Boots (param)", labels_param[labels_param == 1].size))
+        
+        list_summary_perceptron.append(evaluate_perceptron(sample_size, features_param, labels_param))
+        list_summary_SVM.append(evaluate_SVM(sample_size, features_param, labels_param))
+        list_summary_knearest.append(evaluate_knearest(sample_size, features_param, labels_param))
+        list_summary_dtree.append(evaluate_dtree(sample_size, features_param, labels_param))
     
     
     #------------------------------------------------------------------------------------
@@ -208,11 +229,7 @@ def main():
     #------------------------------------------------------------------------------------
     # COMPLETED    -  Use the procedure developed in task 2 to train and evaluate the Perceptron classifier [1 point]
     # COMPLETED    -  What is the mean prediction accuracy of this classifier [1 point]
-    # TODO    -  Vary the number of samples and plot the relationship between input data size and runtimes for the classifier [1 point].
-    summary_perceptron = evaluate_perceptron(features_param, labels_param)
-    
-    
-    
+    # COMPLETED    -  Vary the number of samples and plot the relationship between input data size and runtimes for the classifier [1 point].
     
     
     #------------------------------------------------------------------------------------
@@ -221,13 +238,9 @@ def main():
     # COMPLETED    -  Use the procedure developed in task 2 to train and evaluate the Support Vector Machine classifier [1 point]
     # COMPLETED    -  Use a radial basis function kernel and try different choices for the parameter 𝛾 [1 point]
     # COMPLETED    -  Determine a good value for 𝛾 based on mean prediction accuracy [1 point]
-    # TODO    -  What is the best achievable mean prediction accuracy of this classifier [1 point]
+    # COMPLETED    -  What is the best achievable mean prediction accuracy of this classifier [1 point]
     # COMPLETED    -  Determine a good value for 𝛾 based on mean prediction accuracy [1 point]
-    # TODO    -  Vary the number of samples and plot the relationship between input data size and runtimes for the optimal classifier [1 point]
-    summary_SVM = evaluate_SVM(features_param, labels_param)
-    
-    
-    
+    # COMPLETED    -  Vary the number of samples and plot the relationship between input data size and runtimes for the optimal classifier [1 point]
     
     
     #------------------------------------------------------------------------------------
@@ -237,43 +250,78 @@ def main():
     # COMPLETED    -  Try different choices for the parameter k [1 point]
     # COMPLETED    -  and determine a good value based on mean prediction accuracy [1 point]
     # COMPLETED    -  What is the best achievable mean prediction accuracy of this classifier [1 point]
-    # TODO    -  Vary the number of samples and plot the relationship between input data size and runtimes for the optimal classifier [1 point]
-    summary_knearest = evaluate_knearest(features_param, labels_param)
-    
-    
-    
-    
+    # COMPLETED    -  Vary the number of samples and plot the relationship between input data size and runtimes for the optimal classifier [1 point]
     
     
     #------------------------------------------------------------------------------------
     #--------------------------- TASK 6 (Decision trees, 3 points) --------------------------
     #------------------------------------------------------------------------------------
-    # TODO    -  Vary the number of samples and plot the relationship between input data size and runtimes for the optimal classifier [1 point]
-    # TODO    -  What is the mean prediction accuracy of this classifier [1 point]
-    # TODO    -  Vary the number of samples and plot the relationship between input data size and runtimes for the classifier [1 point]
-    summary_dtree = evaluate_dtree(features_param, labels_param)
-    
-    
-    
+    # COMPLETED    -  Vary the number of samples and plot the relationship between input data size and runtimes for the optimal classifier [1 point]
+    # COMPLETED    -  What is the mean prediction accuracy of this classifier [1 point]
+    # COMPLETED    -  Vary the number of samples and plot the relationship between input data size and runtimes for the classifier [1 point]
     
     
     #------------------------------------------------------------------------------------
     #--------------------------- TASK 7 (comparison, 5 points) --------------------------
     #------------------------------------------------------------------------------------
-    # TODO    -  Compare the training and prediction times of the four classifiers. What trend do you observe for each of the classifiers and why [4 points]
+    # COMPLETED    -  Compare the training and prediction times of the four classifiers. What trend do you observe for each of the classifiers and why [4 points]
     # TODO    -  Also taking the accuracy into consideration, how would you rank the four classifiers and why [1 point]
     print("\n\n--------------------------------------------------")
-    print("\n------------------- COMPARISON -------------------")
+    print("\n------------------- Comparison")
     print("\n--------------------------------------------------")
     
-    print("{:<16s}:\t{:>}".format("Classifier", "Perceptron"))
-    print(summary_perceptron)
-    #plt.plot(summary_perceptron['training_times'], )
-    #plt.show()
     
-    print("\nSummary SVM", summary_SVM)
-    print("\nSummary K-Nearest Neighbour", summary_knearest)
-    print("\nSummary Decision Tree", summary_dtree)
+    # Comparing Classifier Prediction Accuracies
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_accuracies'])for s in list_summary_perceptron)), label='Perceptron', color='red')
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_accuracies'])for s in list_summary_SVM)), label='Support Vector Machine (gamma=1e-07)', color='green')
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_accuracies'])for s in list_summary_knearest)), label='K-Nearest Neighbour (n_neighbors=3)', color='blue')
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_accuracies'])for s in list_summary_dtree)), label='Decision Tree', color='orange')
+    plt.title("Comparing Classifier Prediction Accuracies")
+    plt.xlabel("Sample Size")
+    plt.ylabel("Prediction Accuracy (%)")
+    plt.legend()
+    plt.show()
+    
+    
+    
+    
+    # Comparing Classifier Processing Times
+    figure(figsize=(10,12),dpi=80)
+    
+    plt.subplot(2, 2, 1)
+    plt.plot(SAMPLE_SIZES, (list(avg(s['training_times'])for s in list_summary_perceptron)), label="Training")
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_times'])for s in list_summary_perceptron)), label="Prediction")
+    plt.title("Perceptron")
+    plt.xlabel("Sample Size")
+    plt.ylabel("Processing time (ms)")
+    plt.legend()
+    
+    plt.subplot(2, 2, 2)
+    plt.plot(SAMPLE_SIZES, (list(avg(s['training_times'])for s in list_summary_SVM)), label="Training")
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_times'])for s in list_summary_SVM)), label="Prediction")
+    plt.title("Support Vector Machine (gamma=1e-07)")
+    plt.xlabel("Sample Size")
+    plt.ylabel("Processing time (ms)")
+    plt.legend()
+    
+    plt.subplot(2, 2, 3)
+    plt.plot(SAMPLE_SIZES, (list(avg(s['training_times'])for s in list_summary_knearest)), label="Training")
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_times'])for s in list_summary_knearest)), label="Prediction")
+    plt.title("K-Nearest Neighbour (n_neighbors=3)")
+    plt.xlabel("Sample Size")
+    plt.ylabel("Processing time (ms)")
+    plt.legend()
+    
+    plt.subplot(2, 2, 4)
+    plt.plot(SAMPLE_SIZES, (list(avg(s['training_times'])for s in list_summary_dtree)), label="Training")
+    plt.plot(SAMPLE_SIZES, (list(avg(s['prediction_times'])for s in list_summary_dtree)), label="Prediction")
+    plt.title("Decision Tree")
+    plt.xlabel("Sample Size")
+    plt.ylabel("Processing time (ms)")
+    plt.legend()
+    
+    plt.suptitle("Comparing Classifier Processing Times")
+    plt.show()
     
 
 main()
